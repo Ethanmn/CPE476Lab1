d380fd09ba5160f6fa584eda942df7bf256afa4f
Ethan Nakashima <Ethanmn@hotmail.com>
2014-04-10 13:26:22 -0700

    Added GameObject stuff

---
+++ Test_Again/GameObject.h
@@ -0,0 +1,13 @@
+#ifndef GAMEOBJECT_H_
+#define GAMEOBJECT_H_
+
+#include <assimp/Importer.hpp>      // C++ importer interface
+#include <assimp/scene.h>           // Output data structure
+#include <assimp/postprocess.h>     // Post processing flags
+
+class GameObject {
+    public:
+      bool 
+}
+
+#endif //GAMEOBJECT_H_
